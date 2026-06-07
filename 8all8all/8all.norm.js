e0d1748e7d4ea5379a4ef2841da60f903865329f
iki1uc <entertrainer@web.de>
2026-06-07 20:06:12 +0200

    Update 8all.norm.js

--- 8all8all/8all.norm.js
+++ 8all8all/8all.norm.js
@@ -1,1 +1,9 @@
+// 8ALL NORM – TECH-Stand & Normen
+export const EightAllNorm = {
+  version: "1.0",
+  required: ["tech", "norm", "respo"],
+  validate(obj) {
+    return this.required.every(key => key in obj);
+  }
+};
 
