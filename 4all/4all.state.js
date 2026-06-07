8e402a65e3cea0c8254a191555524737de4c6776
iki1uc <entertrainer@web.de>
2026-06-07 20:02:12 +0200

    Update 4all.state.js

--- 4all/4all.state.js
+++ 4all/4all.state.js
@@ -1,1 +1,11 @@
+// 4ALL – State Container
+export const FourAllState = {
+  current: "NEU",
+  history: [],
+  set(state) {
+    this.history.push(this.current);
+    this.current = state;
+    console.log("4ALL STATE →", state);
+  }
+};
 
