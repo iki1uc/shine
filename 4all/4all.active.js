f3e7738a3e15ef9e08290e396e0ac9cb98967eeb
iki1uc <entertrainer@web.de>
2026-06-07 20:01:56 +0200

    Update 4all.active.js

--- 4all/4all.active.js
+++ 4all/4all.active.js
@@ -1,1 +1,17 @@
+// 4ALL – System-Manager (IST/SOLL)
+export const FourAll = {
+  ist: {
+    respo: "NEU",
+    version: "1.0.0"
+  },
+  soll: {
+    respo: "NEU",
+    version: "1.0.0"
+  },
+  status() {
+    console.log("4ALL IST:", this.ist);
+    console.log("4ALL SOLL:", this.soll);
+    return this.ist;
+  }
+};
 
