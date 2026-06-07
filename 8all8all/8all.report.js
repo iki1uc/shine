d8c6a7f1609b5ccbb0441e9b221199e91958ab6c
iki1uc <entertrainer@web.de>
2026-06-07 20:06:32 +0200

    Update 8all.report.js

--- 8all8all/8all.report.js
+++ 8all8all/8all.report.js
@@ -1,1 +1,9 @@
+// 8ALL REPORT – einfache Ausgabe
+import { EightAll } from "./8all.core.js";
+
+export function eightAllReport() {
+  const status = EightAll.check();
+  console.log("8ALL REPORT:", status);
+  return status;
+}
 
